70f5e1f05a6fdae75378cbf7f3a390e9503133dd
Alper Hardal <agustefatu@gmail.com>
2021-08-06 15:44:49 +0300

    hello

---
+++ cs50web/lecture1/urls.py
@@ -0,0 +1,20 @@
+from unicodedata import name
+from django.urls import URLPattern
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("hello",views.hello,name="hello"),
+    path("headings",views.headings,name="headings"),
+    path("lists",views.lists,name="lists"),
+    path("image",views.image,name="image"),
+    path("link",views.link,name="link"),
+    path("table",views.table,name="table"),
+    path("form",views.form,name="form"),
+
+
+
+# Give url 'alper' , give run function 'alper function insice views ' and a name 
+
+]
---
+++ cs50web/lecture1/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'lecture1/index.html')
+    
+def hello(request):
+    return render(request,'lecture1/hello.html')
+
+def headings(request):
+    return render (request,'lecture1/headings.html')
+
+def lists(request):
+    return render (request,'lecture1/lists.html')
+
+def image(request):
+    return render (request,'lecture1/image.html')
+
+def link(request):
+    return render (request,'lecture1/link.html')
+
+def table(request):
+    return render (request,'lecture1/table.html')
+
+def form(request):
+    return render (request,'lecture1/form.html')
+
+
+
+
+def alper(request):
+    return HttpResponse("Hello Alper!")
